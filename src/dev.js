93a1a64d04f7758c9fde00301a21229e48b36cf2
deizel <web@deizel.co.uk>
2014-02-21 13:45:03 +0000

    Separate buttons to toggle column and toggle outlines.

--- src/dev.js
+++ src/dev.js
@@ -10,21 +10,39 @@
 	var path = src.replace(/\w+.js$/, '');
 
 	$(document).ready(function () {
-		$('head').append('<link rel="stylesheet" href="' + path + 'dev.css" type="text/css" />');
-		$('body').prepend('<div class="container developer"><button type="button" class="toggle btn btn-info btn-xs"><i class="glyphicon glyphicon-th"></i></button><div class="row developer-columns" ><div class="col-md-1">&nbsp;</div><div class="col-md-1">&nbsp;</div><div class="col-md-1">&nbsp;</div><div class="col-md-1">&nbsp;</div><div class="col-md-1">&nbsp;</div><div class="col-md-1">&nbsp;</div><div class="col-md-1">&nbsp;</div><div class="col-md-1">&nbsp;</div><div class="col-md-1">&nbsp;</div><div class="col-md-1">&nbsp;</div><div class="col-md-1">&nbsp;</div><div class="col-md-1">&nbsp;</div></div></div>');
-		$('.toggle').click(function () {
-			$('.developer-columns').toggle(0, function () {
-				if ($(this).is(':visible')) {
-					$('div[class*="col-lg-"]').each(function () {
-						$(this).addClass('dev-highlight');
-					});
-				} else {
-					$('div.dev-highlight').each(function () {
-						$(this).removeClass('dev-highlight');
-					});
-				}
-			});
+
+		var stylesheet = '<link rel="stylesheet" href="' + path + 'dev.css" type="text/css" />';
+		$('head').append(stylesheet);
+
+		var buttonColumns = '<button class="dev-toggle-columns btn btn-primary btn-xs"><i class="glyphicon glyphicon-barcode"></i></button>';
+		var buttonOutline = '<button class="dev-toggle-outline btn btn-info btn-xs"><i class="glyphicon glyphicon-qrcode"></i></button>';
+		var buttons = '<div class="dev-buttons btn-group">' + buttonColumns + buttonOutline + '</div>'
+		$('body').prepend(buttons);
+
+		var columns = '';
+		for (var i = 0; i < 12; i++) {
+			columns += '<div class="col-md-1"></div>';
+		}
+		columns = '<div class="container dev-container"><div class="row">' + columns + '</div></div>';
+		$('body').prepend(columns);
+
+		$('button.dev-toggle-columns').click(function () {
+			$('.dev-container').toggle();
 		});
+
+		$('button.dev-toggle-outline').click(function () {
+			var outlines = $('div.dev-outline');
+			if (outlines.length === 0) {
+				$('div[class*="col-lg-"]').each(function () {
+					$(this).addClass('dev-outline');
+				});
+			} else {
+				outlines.each(function () {
+					$(this).removeClass('dev-outline');
+				});
+			}
+		});
+
 	});
 
 })();
